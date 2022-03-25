2d9ca91609b7c11c296490b6952116f6dec50a33
lizujun <lizejun@lingxing.com>
2022-03-25 08:55:01 +0800

    feat: 新增ant ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,9 @@
 import React, {Component} from 'react'
 import Login from './Login';
 import admin from './pages/admin';
+import 'antd/dist/antd.css';
 import "./app.css";
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch, BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 	render() {
@@ -10,7 +11,7 @@ class App extends Component {
 			<HashRouter>
 				<Switch>
 					<Route path="/login" component={Login}></Route>
-					<Route path="/" component={admin}></Route>
+					<Route path="/" exact component={admin}></Route>
 				</Switch>
 			</HashRouter>
 		);
