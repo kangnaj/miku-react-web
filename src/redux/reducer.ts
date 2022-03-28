1898d62baf6156b3a8d1058712b2c9de35103913
lizujun <lizejun@lingxing.com>
2022-03-28 09:04:35 +0800

    feat: 新增redux-react

---
+++ src/redux/reducer.ts
@@ -0,0 +1,8 @@
+
+//管理头部标题
+const initHeadTitle = '首页';
+const rootReducer = (state: string = initHeadTitle, action: { type: any; payload: string }): string => {
+  return state;
+};
+
+export default rootReducer;
